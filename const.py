efc089cba45003f5fa98da0fa7500f4de883d769
ofdun <elik394@mail.ru>
2023-06-07 16:05:37 +0300

    added video formats

--- const.py
+++ const.py
@@ -1,2 +1,3 @@
 WELCOME_MESSAGE = "Привет, {name}! Я поздравлю тебя с праздником при помощи открытки!"
 SITE_WITH_POSTCARDS_HREF = 'https://3d-galleru.ru/archive/cat/kalendar-42/'
+VIDEO_FORMATS = ['.mp4', '.webm', '.mov', '.mkv', '.avi', '.flv']
