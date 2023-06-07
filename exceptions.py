849e6ac82fe58a3444cb48e410ade5c53fc20ed2
ofdun <elik394@mail.ru>
2023-06-07 16:05:45 +0300

    added exceptions

---
+++ exceptions.py
@@ -0,0 +1,14 @@
+class NoPictureAvailable(Exception):
+    pass
+
+class NoResponseFromTheSite(Exception):
+    pass
+
+class NoResponseFromPicturesPage(Exception):
+    pass
+
+class NoResponseFromTheHolidaysPage(Exception):
+    pass
+
+class NoResponseFromPicturesDownloadHref(Exception):
+    pass
